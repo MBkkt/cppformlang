7ad289ee53239145d916b26b7bcde25d60fd2587
MBkkt <MBkkt@users.noreply.github.com>
2020-08-18 20:36:49 +0300

    Initial commit, fork from https://github.com/TheLartians/ModernCppStarter

---
+++ test/source/greeter.cpp
@@ -0,0 +1,22 @@
+#include <cppformlang/greeter.h>
+#include <cppformlang/version.h>
+#include <doctest/doctest.h>
+
+#include <string>
+
+TEST_CASE("Greeter") {
+  using namespace greeter;
+
+  Greeter greeter("Tests");
+
+  CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
+  CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
+  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
+  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
+}
+
+TEST_CASE("cppformlang version") {
+  static_assert(std::string_view(CPPFORMLANG_VERSION) ==
+                std::string_view("1.0"));
+  CHECK(std::string(CPPFORMLANG_VERSION) == std::string("1.0"));
+}
